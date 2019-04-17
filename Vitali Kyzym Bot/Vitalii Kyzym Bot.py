49a51943707cb02985721a59bf0681cb0cc20efe
TheToxicMenace <radmanmus@gmail.com>
2019-04-17 16:50:36 +0300

    replaced with gromo bot and added functionality

--- Vitali Kyzym Bot/Vitalii Kyzym Bot.py
+++ Vitali Kyzym Bot/Vitalii Kyzym Bot.py
@@ -1,3 +1,13 @@
-import telepod
-bot = teleport.Bot('874633254:AAEKR5m_ZL6mJ83VQIslrJcastFLaRr96CQ')
-bot.getMe()
+import telepot
+from telepot.loop import MessageLoop
+import time
+
+bot = telepot.Bot('859146216:AAHb_DkF6mQ44aLR9ElHsM0HY1t0OK_way0')
+
+def handle(msg):
+    print(msg["text"])
+
+MessageLoop(bot, handle).run_as_thread()
+
+while 1:
+    time.sleep(1)
