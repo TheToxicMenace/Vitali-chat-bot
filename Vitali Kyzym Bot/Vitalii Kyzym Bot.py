db72972705b5500167241102064cf7c0af140fd1
TheToxicMenace <radmanmus@gmail.com>
2019-05-15 16:53:50 +0300

    added more code

--- Vitali Kyzym Bot/Vitalii Kyzym Bot.py
+++ Vitali Kyzym Bot/Vitalii Kyzym Bot.py
@@ -5,6 +5,8 @@ import time
 bot = telepot.Bot('874633254:AAEKR5m_ZL6mJ83VQIslrJcastFLaRr96CQ')
 
 def handle(msg):
+    if msg["text"] == "Hi":
+        print("get out of my face")
     print(msg["text"])
 
 MessageLoop(bot, handle).run_as_thread()
