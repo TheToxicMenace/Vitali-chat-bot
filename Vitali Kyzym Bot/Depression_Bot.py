b6df028813648df6aa4be9e3ef44e7b8d9f05d71
TheToxicMenace <radmanmus@gmail.com>
2019-05-29 16:48:43 +0300

    kawaii

--- Vitali Kyzym Bot/Depression_Bot.py
+++ Vitali Kyzym Bot/Depression_Bot.py
@@ -2,12 +2,14 @@ import telepot
 from telepot.loop import MessageLoop
 import time
 
-bot = telepot.Bot('859146216:AAHb_DkF6mQ44aLR9ElHsM0HY1t0OK_way0')  
+bot = telepot.Bot('874633254:AAEKR5m_ZL6mJ83VQIslrJcastFLaRr96CQ')  
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    if msg["text"] == "":
-        bot.sendMessage(chat_id, 'chicken')
+    if msg["text"] == "Hi":
+        bot.sendMessage(chat_id, 'Hello!')
+    elif msg["text"] == "bye" :
+        bot.sendMessage(chat_id, 'Bai Kawaii Chan!')
 
 MessageLoop(bot, handle).run_as_thread()
 
